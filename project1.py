8bd471fea06222d0abb0c7c78906824f69dfa42a
Yichen Wu <wyc20020111@gmail.com>
2018-01-26 15:29:49 -0800

    init2

--- project1.py
+++ project1.py
@@ -1,1 +1,9 @@
-print("hello")
+from sklearn.datasets import fetch_20newsgroups
+
+categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
+              'comp.sys.mac.hardware', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
+
+cat_comp = categories[:4]
+cat_rec = categories[4:]
+
+graphics_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
