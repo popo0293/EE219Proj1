997f03f0f259be6ed4412b8e9b555c3ba8797b37
Yichen Wu <wyc20020111@gmail.com>
2018-01-26 16:51:29 -0800

    hist

--- project1.py
+++ project1.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_20newsgroups
 
 categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
@@ -5,5 +7,19 @@ categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardw
 
 cat_comp = categories[:4]
 cat_rec = categories[4:]
+cat_sum = []
+data = []
 
-graphics_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+for i in range(len(categories)):
+    temp = fetch_20newsgroups(categories=[categories[i]], shuffle=True, random_state=42)
+    data.append(temp)
+    cat_sum.append(len(temp.data))
+
+print(cat_sum)
+
+plt.figure(1)
+plt.title("Size of data set for each class")
+plt.xlabel(categories)
+plt.ylabel("Numbers")
+
+plt.show(block=True)
