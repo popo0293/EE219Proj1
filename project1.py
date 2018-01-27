cec98ed1781518bbf849edc33d0f408a0de1566f
Yichen Wu <wyc20020111@gmail.com>
2018-01-26 18:51:22 -0800

    hist_finished5

--- project1.py
+++ project1.py
@@ -2,18 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.decomposition import NMF
+
+
+class SparseToDenseArray(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def transform(self, X, *_):
+        if hasattr(X, 'toarray'):
+            return X.toarray()
+        return X
+
+    def fit(self, *_):
+        return self
+
+
+# globals
 
 categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
               'comp.sys.mac.hardware', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
 
-cat_comp = categories[:4]
-cat_rec = categories[4:]
-cat_sum = []
-data = []
+cat_comp = categories[:4]   # Computer Technologies
+cat_rec = categories[4:]    # Recreational Activities
+cat_sum = []                # training data size of each class 0-7
+train_data = []             # training data of each class 0-7
 
 for i in range(len(categories)):
     temp = fetch_20newsgroups(subset='train', categories=[categories[i]], shuffle=True, random_state=42)
-    data.append(temp)
+    train_data.append(temp)
     cat_sum.append(len(temp.data))
 
 
@@ -37,6 +57,11 @@ def part_a2():
     pl.xlabel("Training size per class")
     pl.ylabel("Numbers")
     pl.show(block=True)
+
+
+def part_b():
+    pass
+
 
 part_a()
 part_a2()
