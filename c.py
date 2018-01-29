d326143a538caf81ed25e771bb57778f983e4fa7
Yichen Wu <wyc20020111@gmail.com>
2018-01-28 20:33:58 -0800

    c finished added report

--- c.py
+++ c.py
@@ -23,16 +23,14 @@ print("With min_df = %d, (training documents, terms extracted): " % MIN_DF, M_tr
 duration = timer()-start
 print("Computation Time in secs: ", duration)
 
-classes = []
-cat4 = ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
-for i in range(len(cat4)):
-    classes.append(all_data.target_names.index(categories[i]))
-
-for i in range(len(cat4)):
-    arr = np.array(M_train_tficf.toarray()[classes[i], :])
-    max_term = arr.argsort()[-10:][::-1]
-    print("Top 10 terms in %s" % categories[i])
-    for j in max_term:
-        print(list(vectorizer.vocabulary_.keys())[j])
+cat_top10 = ["comp.sys.ibm.pc.hardware", "comp.sys.mac.hardware", "misc.forsale", "soc.religion.christian"]
+print("With min_df = %d" % MIN_DF)
+for name in cat_top10:
+    index = all_data.target_names.index(name)
+    arr = M_train_tficf.toarray()[index, 1:10]
+    sig_terms = np.argsort(arr)[-10:][-1::-1]
+    print("Top 10 most significant terms in class %s are:" % name)
+    for i in range(10):
+        print(list(vectorizer.vocabulary_.keys())[i])
 
 logging.info("finished Problem c")
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = [(),(),(),()]
+sig_terms = np.argsort(arr, order=('y', 'x'))
