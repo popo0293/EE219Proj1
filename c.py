59f1521fa12a1409a8b599566390367ae9fc5f4a
Yichen Wu <wyc20020111@gmail.com>
2018-01-28 17:21:50 -0800

    finished b

---
+++ c.py
@@ -0,0 +1,8 @@
+from global_data import *
+from timeit import default_timer as timer
+from utils import *
+
+logging.debug("Problem c")
+
+
+logging.debug("finished Problem c")
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import nltk
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import RegexpTokenizer
 
-nltk.download("stopwords")
+nltk.download("stopwords")  # if the host does not have the package
 
 
 class SparseToDenseArray(BaseEstimator, TransformerMixin):
